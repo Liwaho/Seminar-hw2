b06df1c02c7d58739d1827ef51e650d2fe8d2bdf
Liwaho Xu <lwhollow@yandex.ru>
2022-09-26 15:51:27 +0500

    Third task

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ Console.WriteLine("Вторая цифра в числе " + number + " - " + nu
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6
-*/Console.Write("Введите число: ");
+*/
+/*Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 string number2 = Convert.ToString(number); //для подсчёта кол-ва цифр
 if (number2.Length > 2) //если длина string больше двух
@@ -21,9 +22,20 @@ if (number2.Length > 2) //если длина string больше двух
     Console.WriteLine("Третья цифра числа " + number + " - " + number % 10);
 }
 else Console.WriteLine("Третьей цифры нет.");
+*/
 
 /*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 6 -> да
 7 -> да
 1 -> нет
 */
+
+Console.Write("Введите число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7){
+    Console.WriteLine(day + "й день - выходной");
+}
+if (day > 7){
+    Console.WriteLine("Неверная цифра.");
+}
+else Console.WriteLine(day + " - будний");
